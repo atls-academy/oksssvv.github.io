b63915c4d76538620aa81db2a151d4ea83ec362f
oksssvv <oksss.vv@gmail.com>
2023-04-01 13:49:46 +0300

    refactor: add workspace

--- auth/src/checks.ts
+++ auth/src/checks.ts
@@ -1,9 +1,9 @@
 import { user }        from './index'
 import { credentials } from './index'
-import { login }        from './login-function'
-import { logout }       from './logout-function'
+import { login }       from './login-function'
+import { logout }      from './logout-function'
 import { register }    from './register-function'
-import { whoami }       from './whoami-function'
+import { whoami }      from './whoami-function'
 
 register(user, credentials, 'Admin', '101010')
 login(user, credentials, 'Admin', '101010')
