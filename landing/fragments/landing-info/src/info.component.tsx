391269e52a5ff38d6dc0512445a934fbe226cc29
oksssvv <oksss.vv@gmail.com>
2023-05-17 20:11:46 +0300

    chore: remove wrap in text

--- landing/fragments/landing-info/src/info.component.tsx
+++ landing/fragments/landing-info/src/info.component.tsx
@@ -14,129 +14,144 @@ const Info = () => (
     <Box>
       <Layout flexBasis={[20, 230]} flexShrink='0' />
       <Column>
-        <Box width={['335px', '1460px']} flexShrink='0'>
+        <Box width={['335px', '1460px']} flexShrink='0' display='inline'>
+          <Text color='text.indigo' fontSize={['major', 'great']} lineHeight='medium'>
+            <FormattedMessage id='landing_info.digital' defaultMessage='Digital' />
+          </Text>
+          <Space />
           <Text
             display='inline'
-            fontWeight='normal'
+            color='text.white'
             fontSize={['major', 'great']}
             lineHeight='medium'
           >
-            <Text color='text.indigo' fontSize={['major', 'great']}>
-              <FormattedMessage id='landing_info.digital' defaultMessage='Digital' />
-            </Text>
-            <Space />
-            <Text display='inline' color='text.white' fontSize={['major', 'great']}>
-              <FormattedMessage
-                id='landing_info.getting_more_crowded'
-                defaultMessage='становится всё более многолюдным, востребованность по направлениям при этом не снижается'
-              />
-            </Text>
-            <Space />
-            <Box
-              display='inline-flex'
-              width={['52px', '76px']}
-              height={['36px', '52px']}
-              backgroundColor='background.transparent'
-              borderRadius={['10px', '12px']}
-              justifyContent='center'
-              alignItems='center'
-            >
-              <Box width={[28, 40]} height={[28, 40]}>
-                <ImageBlock src='/info-icon-sphere.png' />
-              </Box>
-            </Box>
-            <Space />
-            <Text display='inline' color='text.white' fontSize={['major', 'great']}>
-              <FormattedMessage id='landing_info.but' defaultMessage=' Но, ' />
-            </Text>
-            <Space />
-            <Text color='text.indigo' fontSize={['major', 'great']}>
-              <FormattedMessage id='landing_info.specialists' defaultMessage='специалистов' />
-            </Text>
-            <Text display='inline' color='text.white' fontSize={['major', 'great']}>
-              <FormattedMessage
-                id='landing_info.ready_take_responsibility'
-                defaultMessage=', готовых нести ответсвенность за свои решения по прежднему мало'
-              />
-            </Text>
-            <Space />
-            <Box
-              display='inline-flex'
-              width={['52px', '76px']}
-              height={['36px', '52px']}
-              backgroundColor='background.transparent'
-              borderRadius={['10px', '12px']}
-              justifyContent='center'
-              alignItems='center'
-            >
-              <Box width={[28, 40]} height={[28, 40]}>
-                <ImageBlock src='/info-icon-sphere.png' />
-              </Box>
-            </Box>
+            <FormattedMessage
+              id='landing_info.getting_more_crowded'
+              defaultMessage='становится всё более многолюдным, востребованность по направлениям при этом не снижается'
+            />
           </Text>
+          <Space />
+          <Box
+            display='inline-flex'
+            width={['52px', '76px']}
+            height={['36px', '52px']}
+            backgroundColor='background.transparent'
+            borderRadius={['10px', '12px']}
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Box width={[28, 40]} height={[28, 40]}>
+              <ImageBlock src='/info-icon-sphere.png' />
+            </Box>
+          </Box>
+          <Space />
+          <Text
+            display='inline'
+            color='text.white'
+            fontSize={['major', 'great']}
+            lineHeight='medium'
+          >
+            <FormattedMessage id='landing_info.but' defaultMessage=' Но, ' />
+          </Text>
+          <Space />
+          <Text color='text.indigo' fontSize={['major', 'great']} lineHeight='medium'>
+            <FormattedMessage id='landing_info.specialists' defaultMessage='специалистов' />
+          </Text>
+          <Text
+            display='inline'
+            color='text.white'
+            fontSize={['major', 'great']}
+            lineHeight='medium'
+          >
+            <FormattedMessage
+              id='landing_info.ready_take_responsibility'
+              defaultMessage=', готовых нести ответсвенность за свои решения по прежднему мало'
+            />
+          </Text>
+          <Space />
+          <Box
+            display='inline-flex'
+            width={['52px', '76px']}
+            height={['36px', '52px']}
+            backgroundColor='background.transparent'
+            borderRadius={['10px', '12px']}
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Box width={[28, 40]} height={[28, 40]}>
+              <ImageBlock src='/info-icon-sphere.png' />
+            </Box>
+          </Box>
         </Box>
         <Layout flexBasis={[24, 40]} flexShrink='0' />
-        <Box width={['335px', '1460px']}>
+        <Box width={['335px', '1460px']} display='inline'>
           <Text
-            display='inline'
-            fontWeight='normal'
+            color={['text.white', 'text.indigo']}
             fontSize={['major', 'great']}
             lineHeight='medium'
           >
-            <Text color={['text.white', 'text.indigo']} fontSize={['major', 'great']}>
-              <FormattedMessage
-                id='landing_info.atlantis_academy'
-                defaultMessage='Atlantis Academy'
-              />
-            </Text>
-            <Space />
-            <Box display={['none', 'inline']}>
-              <Text display='inline' color='text.white' fontSize='great'>
-                <FormattedMessage
-                  id='landing_info.teaches_and_shapes'
-                  defaultMessage='обучает и формирует'
-                />
-              </Text>
-            </Box>
-            <Space />
-            <Box display={['inline', 'none']}>
-              <Text display='inline' color='text.white' fontSize='major'>
-                <FormattedMessage id='landing_info.shapes' defaultMessage='формирует' />
-              </Text>
-              <Space />
-            </Box>
-            <Text color='text.indigo' fontSize={['major', 'great']}>
-              <FormattedMessage id='landing_info.engineers' defaultMessage='инженеров' />
-            </Text>
-            <Space />
-            <Text display='inline' color='text.white' fontSize={['major', 'great']}>
-              <FormattedMessage id='landing_info.and' defaultMessage='и' />
-            </Text>
-            <Space />
-            <Text color='text.indigo' fontSize={['major', 'great']}>
-              <FormattedMessage id='landing_info.architects' defaultMessage='архитекторов' />
-            </Text>
-            <Text display='inline' color='text.white' fontSize={['major', 'great']}>
-              <FormattedMessage
-                id='landing_info.not_programmers_and_designers'
-                defaultMessage=', а не программистов и дизайнеров'
-              />
-            </Text>
-            <Space />
-            <Box
-              display='inline-flex'
-              width={['52px', '76px']}
-              height={['36px', '52px']}
-              backgroundColor='background.transparent'
-              borderRadius={['10px', '12px']}
-              justifyContent='center'
-              alignItems='center'
-            >
-              <Box width={[28, 40]} height={[28, 40]}>
-                <ImageBlock src='/info-icon-sphere.png' />
-              </Box>
-            </Box>
+            <FormattedMessage
+              id='landing_info.atlantis_academy'
+              defaultMessage='Atlantis Academy'
+            />
           </Text>
+          <Space />
+          <Box display={['none', 'inline']}>
+            <Text display='inline' color='text.white' fontSize='great' lineHeight='medium'>
+              <FormattedMessage
+                id='landing_info.teaches_and_shapes'
+                defaultMessage='обучает и формирует'
+              />
+            </Text>
+          </Box>
+          <Space />
+          <Box display={['inline', 'none']}>
+            <Text display='inline' color='text.white' fontSize='major' lineHeight='medium'>
+              <FormattedMessage id='landing_info.shapes' defaultMessage='формирует' />
+            </Text>
+            <Space />
+          </Box>
+          <Text color='text.indigo' fontSize={['major', 'great']} lineHeight='medium'>
+            <FormattedMessage id='landing_info.engineers' defaultMessage='инженеров' />
+          </Text>
+          <Space />
+          <Text
+            display='inline'
+            color='text.white'
+            fontSize={['major', 'great']}
+            lineHeight='medium'
+          >
+            <FormattedMessage id='landing_info.and' defaultMessage='и' />
+          </Text>
+          <Space />
+          <Text color='text.indigo' fontSize={['major', 'great']} lineHeight='medium'>
+            <FormattedMessage id='landing_info.architects' defaultMessage='архитекторов' />
+          </Text>
+          <Text
+            display='inline'
+            color='text.white'
+            fontSize={['major', 'great']}
+            lineHeight='medium'
+          >
+            <FormattedMessage
+              id='landing_info.not_programmers_and_designers'
+              defaultMessage=', а не программистов и дизайнеров'
+            />
+          </Text>
+          <Space />
+          <Box
+            display='inline-flex'
+            width={['52px', '76px']}
+            height={['36px', '52px']}
+            backgroundColor='background.transparent'
+            borderRadius={['10px', '12px']}
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Box width={[28, 40]} height={[28, 40]}>
+              <ImageBlock src='/info-icon-sphere.png' />
+            </Box>
+          </Box>
         </Box>
       </Column>
       <Layout flexBasis={[20, 230]} flexShrink='0' />
