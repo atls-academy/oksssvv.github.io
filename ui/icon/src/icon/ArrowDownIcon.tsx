9946766cdcf6fc8bc47349c470b8fbc107c98941
oksssvv <oksss.vv@gmail.com>
2023-06-13 19:13:41 +0300

    feat: create icons for 2 step

---
+++ ui/icon/src/icon/ArrowDownIcon.tsx
@@ -0,0 +1,28 @@
+import { useTheme }  from '@emotion/react'
+
+/* eslint-disable */
+import React         from 'react'
+
+import { IconProps } from '../icons.interfaces'
+
+export const ArrowDownIcon = (props: IconProps) => {
+  const theme: any = useTheme()
+  return (
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 14 8'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M13 1L7 7L1 1'
+        stroke='black'
+        strokeWidth={1.75}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
---
+++ ui/icon/src/icon/ArrowRightIcon.tsx
@@ -0,0 +1,28 @@
+import { useTheme }  from '@emotion/react'
+
+/* eslint-disable */
+import React         from 'react'
+
+import { IconProps } from '../icons.interfaces'
+
+export const ArrowRightIcon = (props: IconProps) => {
+  const theme: any = useTheme()
+  return (
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 8 15'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M1 1.5L7 7.50001L1 13.5'
+        stroke='black'
+        strokeWidth={1.75}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
