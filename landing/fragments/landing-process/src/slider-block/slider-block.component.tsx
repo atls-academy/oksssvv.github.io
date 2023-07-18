7a999ce705721c00e9706062d09aa861e0d28f3c
oksssvv <oksss.vv@gmail.com>
2023-07-18 03:35:13 +0300

    fix: add swiches for slider in process fragment

--- landing/fragments/landing-process/src/slider-block/slider-block.component.tsx
+++ landing/fragments/landing-process/src/slider-block/slider-block.component.tsx
@@ -1,99 +1,24 @@
 import React                   from 'react'
-import { FormattedMessage }    from 'react-intl'
 
-import { Background }          from '@ui/background'
-import { Button }              from '@ui/button'
-import { SmallArrowLeftIcon }  from '@ui/icon'
-import { SmallArrowRightIcon } from '@ui/icon'
-import { Image }               from '@ui/image'
-import { Layout }              from '@ui/layout'
-import { Box }                 from '@ui/layout'
 import { Column }              from '@ui/layout'
 import { Slider }              from '@ui/slider'
-import { TimeLine }            from '@ui/slider'
-import { Text }                from '@ui/text'
 
 import { sliderImagesDesktop } from './slider-block.images'
 import { sliderImagesMobile }  from './slider-block.images'
 
 export const SliderBlock = () => (
-  <>
-    <Background
-      display='flex'
-      width={[155, 960]}
-      height={[323, 540]}
-      backgroundColor='lightGray'
-      position='relative'
-      border='ghostGray'
-      borderRadius={[20, 24]}
-    >
-      <Background
-        width={[148, 936]}
-        height={[315, 516]}
-        display='flex'
-        backgroundColor='radialGray'
-        position='absolute'
-        top={[1, 12]}
-        left={[1, 12]}
-        borderRadius={[15, 12]}
-      >
-        <Column>
-          <Image src='/toolbar.png' display={['none', 'flex']} />
-          <Image src='/mobilePanel.png' display={['flex', 'none']} />
-          <Slider
-            images={sliderImagesDesktop}
-            widthImg='80%'
-            heightImg='100%'
-            display={['none', 'flex']}
-          />
-          <Slider
-            images={sliderImagesMobile}
-            widthImg='80%'
-            heightImg='100%'
-            display={['flex', 'none']}
-          />
-        </Column>
-      </Background>
-    </Background>
-
-    <Layout flexBasis={[26, 32]} />
-    <Box width={[335, 960]} height={[64]} alignItems='center'>
-      <Button size='smallSize' variant='ghost'>
-        <SmallArrowLeftIcon />
-      </Button>
-      <Layout flexBasis={[12, 16]} />
-      <Box
-        position='relative'
-        overflow='hidden'
-        flexDirection='column'
-        width='100%'
-        justifyContent='center'
-        alignItems='center'
-        height={[64]}
-        border='ghostGray'
-        borderRadius={12}
-      >
-        <Layout flexBasis={[28, 0]} />
-        <Box justifyContent='center' width={[175, 700]}>
-          <Text
-            color='white'
-            fontSize={['little', 'middle']}
-            lineHeight={['interim', 'large']}
-            textAlign='center'
-          >
-            <FormattedMessage
-              id='/'
-              defaultMessage='Формирование системы базовых фрагментов и компонентов'
-            />
-          </Text>
-        </Box>
-        <TimeLine />
-        <Layout flexBasis={[28, 0]} />
-      </Box>
-      <Layout flexBasis={[12, 16]} />
-      <Button size='smallSize' variant='ghost'>
-        <SmallArrowRightIcon />
-      </Button>
-    </Box>
-  </>
+  <Column>
+    <Slider
+      images={sliderImagesDesktop}
+      widthImg='80%'
+      heightImg='95%'
+      display={['none', 'flex']}
+    />
+    <Slider
+      images={sliderImagesMobile}
+      widthImg='80%'
+      heightImg='100%'
+      display={['flex', 'none']}
+    />
+  </Column>
 )
--- ui/slider/src/slider.component.tsx
+++ ui/slider/src/slider.component.tsx
@@ -1,40 +1,134 @@
-import React           from 'react'
-import { FC }          from 'react'
-import { motion }      from 'framer-motion'
-import { useEffect }   from 'react'
-import { useState }    from 'react'
+import React                   from 'react'
+import { FC }                  from 'react'
+import { FormattedMessage }    from 'react-intl'
+import { motion }              from 'framer-motion'
+import { useEffect }           from 'react'
+import { useState }            from 'react'
 
-import { Box }         from '@ui/layout'
+import { Background }          from '@ui/background'
+import { Button }              from '@ui/button'
+import { SmallArrowLeftIcon }  from '@ui/icon'
+import { SmallArrowRightIcon } from '@ui/icon'
+import { Image }               from '@ui/image'
+import { Layout }              from '@ui/layout'
+import { Column }              from '@ui/layout'
+import { Box }                 from '@ui/layout'
+import { Text }                from '@ui/text'
 
-import { SliderProps } from './slider.interfaces'
+import { SliderProps }         from './slider.interfaces'
 
 export const Slider: FC<SliderProps> = ({ widthImg, heightImg, images, display }) => {
   const [index, setIndex] = useState(0)
+  const [animation, setAnimation] = useState(0)
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIndex(() => (index + 1) % images.length)
+      setIndex((index + 1) % images.length)
     }, 10000)
     return () => clearTimeout(timer)
   })
 
+  const leftClick = () => {
+    setIndex((index - 1 + images.length) % images.length)
+    setAnimation(animation + 1)
+  }
+
+  const rightClick = () => {
+    setIndex((index + 1) % images.length)
+    setAnimation(animation + 1)
+  }
+
   return (
-    <Box
-      width='100%'
-      justifyContent='center'
-      alignItems='center'
-      overflow='hidden'
-      display={display}
-    >
-      <motion.img
-        key={index}
-        src={images[index]}
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-        transition={{ duration: 1 }}
-        width={widthImg}
-        height={heightImg}
-      />
-    </Box>
+    <Column display={display} alignItems='center'>
+      <Background
+        display='flex'
+        width={[155, 960]}
+        height={[323, 540]}
+        backgroundColor='lightGray'
+        position='relative'
+        border='ghostGray'
+        borderRadius={[20, 24]}
+      >
+        <Background
+          width={[148, 936]}
+          height={[315, 516]}
+          display='flex'
+          backgroundColor='radialGray'
+          position='absolute'
+          top={[1, 12]}
+          left={[1, 12]}
+          borderRadius={[15, 12]}
+        >
+          <Box width='100%' justifyContent='center' alignItems='center' flexDirection='column'>
+            <Image src='/toolbar.png' display={['none', 'flex']} />
+            <Image src='/mobilePanel.png' display={['flex', 'none']} />
+            <motion.img
+              key={index}
+              src={images[index]}
+              animate={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              width={widthImg}
+              height={heightImg}
+            />
+          </Box>
+        </Background>
+      </Background>
+
+      <Layout flexBasis={[26, 32]} />
+      <Box width={[335, 960]} height={[64]} alignItems='center'>
+        <Button size='smallSize' variant='ghost' onClick={leftClick}>
+          <SmallArrowLeftIcon />
+        </Button>
+        <Layout flexBasis={[12, 16]} />
+        <Box
+          position='relative'
+          overflow='hidden'
+          flexDirection='column'
+          width='100%'
+          justifyContent='center'
+          alignItems='center'
+          height={64}
+          border='ghostGray'
+          borderRadius={12}
+        >
+          <Layout flexBasis={[28, 0]} />
+          <Box justifyContent='center' width={[175, 700]}>
+            <Text
+              color='white'
+              fontSize={['little', 'middle']}
+              lineHeight={['interim', 'large']}
+              textAlign='center'
+            >
+              <FormattedMessage
+                id='/'
+                defaultMessage='Формирование системы базовых фрагментов и компонентов'
+              />
+            </Text>
+          </Box>
+
+          <Box width='100%' justifyContent='center' position='absolute' top={55}>
+            <motion.div
+              style={{
+                height: '5px',
+                width: '0',
+                backgroundColor: 'white',
+                marginTop: '0',
+                transform: 'translate(0%, 100%)',
+              }}
+              animate={{ width: '100%' }}
+              transition={{ duration: 10, repeat: Infinity }}
+              key={animation}
+            />
+          </Box>
+
+          <Layout flexBasis={[28, 0]} />
+        </Box>
+        <Layout flexBasis={[12, 16]} />
+        <Button size='smallSize' variant='ghost' onClick={rightClick}>
+          <SmallArrowRightIcon />
+        </Button>
+      </Box>
+    </Column>
   )
 }
