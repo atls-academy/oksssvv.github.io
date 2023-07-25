b3bc740bb4dd46351f88bf20916fec778e517ac4
oksssvv <oksss.vv@gmail.com>
2023-07-25 17:39:24 +0300

    chore: rm headlines

--- landing/fragments/landing-library/src/materials/data/index.ts
+++ landing/fragments/landing-library/src/materials/data/index.ts
@@ -1,2 +1,1 @@
-export * from './headlines'
 export * from './technologies'
--- landing/fragments/landing-library/src/materials/materials.component.tsx
+++ landing/fragments/landing-library/src/materials/materials.component.tsx
@@ -1,4 +1,5 @@
 import React            from 'react'
+import { useIntl }       from 'react-intl'
 
 import { Divider }      from '@ui/divider'
 import { Box }          from '@ui/layout'
@@ -6,33 +7,39 @@ import { Column }       from '@ui/layout'
 import { Layout }       from '@ui/layout'
 import { Text }         from '@ui/text'
 
-import { Headlines }    from './data'
 import { Technologies } from './data'
 import { Item }         from './item'
 
-export const Materials = (divider) => (
-  <>
-    {Headlines.map((title) => (
+export const Materials = (divider) => {
+  const intl = useIntl()
+  const technology = Technologies.map((item) => (
+    <Column key={item.index}>
+      <Box border='ghostPrimary' borderRadius='little' padding='microYSmallX' marginRight='micro'>
+        <Text color='text.blueDark' fontSize='little' textTransform='uppercase'>
+          {item.name}
+        </Text>
+      </Box>
+      <Layout flexBasis={12} />
+    </Column>
+  ))
+
+  return (
+    <>
       <Item
-        key={title.index} /*eslint-disable-line */
-        title={title.name}
-        technology={Technologies.map((technology) => (
-          <Column key={technology.index}>
-            <Box
-              border='ghostPrimary'
-              borderRadius='little'
-              padding='microYSmallX'
-              marginRight='micro'
-            >
-              <Text color='text.blueDark' fontSize='little' textTransform='uppercase'>
-                {technology.name}
-              </Text>
-            </Box>
-            <Layout flexBasis={12} />
-          </Column>
-        ))}
+        title={intl.formatMessage({ id: 'library.title.design-figma-math' })}
+        technology={technology}
       />
-    ))}
-    <Divider weight={divider} backgroundColor='background.ghostGray' />
-  </>
-)
+      <Divider weight={divider} backgroundColor='background.ghostGray' />
+      <Item
+        title={intl.formatMessage({ id: 'library.title.how-use-libraries' })}
+        technology={technology}
+      />
+      <Divider weight={divider} backgroundColor='background.ghostGray' />
+      <Item
+        title={intl.formatMessage({ id: 'library.hexagonal-architecture' })}
+        technology={technology}
+      />
+      <Divider weight={divider} backgroundColor='background.ghostGray' />
+    </>
+  )
+}
